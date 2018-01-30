6ce666477b88887effe8a33d551a24371effe735
filedesless <filedesless@gmail.com>
2018-01-29 19:18:54 -0500

    lil type annotation

--- 03.ml
+++ 03.ml
@@ -2,7 +2,7 @@ let solution =
   let max = 600851475143 in
 
   let largest_prime_factor (n: int): int =
-    let rec aux d i =
+    let rec aux (d: int) (i: int): int =
       if i = d then i else
       if i mod d = 0
       then aux d (i / d)
