1c8e4424c52667aa1a0e9ac1d197a105dc5b6a94
filedesless <filedesless@gmail.com>
2019-01-22 15:11:36 -0500

    prob 3

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,12 @@ module Main where
 
 import P001
 import P002
+import P003
 
 format :: Show a => (Integer, a) -> String
 format (i, v) = "Problem " ++ show i ++ ": " ++ show v
 
-solved = [p001, p002]
+solved = [p001, p002, p003]
 
 main :: IO ()
 main = do
