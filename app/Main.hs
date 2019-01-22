1387b19e3014b87f76cc4bba20bc59e223e73769
filedesless <filedesless@gmail.com>
2019-01-22 17:43:17 -0500

    fix main

--- app/Main.hs
+++ app/Main.hs
@@ -6,11 +6,12 @@ import P003
 import P004
 import P005
 import P006
+import P007
 
 format :: Show a => (Integer, a) -> String
 format (i, v) = "Problem " ++ show i ++ ": " ++ show v
 
-solved = [p001, p002, p003, p004, p005, p006]
+solved = [p001, p002, p003, p004, p005, p006, p007]
 
 main :: IO ()
 main = do
