6ed754f3cfe5460134fe0ed12b27913b1d20eccf
filedesless <filedesless@gmail.com>
2019-01-22 18:12:50 -0500

    prob 8

--- app/Main.hs
+++ app/Main.hs
@@ -7,11 +7,12 @@ import P004
 import P005
 import P006
 import P007
+import P008
 
 format :: Show a => (Integer, a) -> String
 format (i, v) = "Problem " ++ show i ++ ": " ++ show v
 
-solved = [p001, p002, p003, p004, p005, p006, p007]
+solved = [p001, p002, p003, p004, p005, p006, p007, p008]
 
 main :: IO ()
 main = do
