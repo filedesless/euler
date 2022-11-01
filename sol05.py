41bfc9ca7052188f8b66f203420dd067fb00c55a
Félix Larose-Gervais <filedesless@gmail.com>
2022-11-01 15:40:50 -0400

    other solution using lcm and gcd

--- sol05.py
+++ sol05.py
@@ -3,6 +3,7 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
 from math import prod, sqrt
+from bench import bench
 
 
 def is_prime(n):
@@ -37,7 +38,27 @@ def prime_fact(n):
 assert prime_fact(100) == {2: 2, 5: 2}
 
 
-def solve(n):
+def gcd(a, b):
+    """Euclidean GCD"""
+    if b == 0:
+        return a
+    if a < b:
+        return gcd(b, a)
+    return gcd(b, a % b)
+
+
+assert gcd(18, 48) == 6
+
+
+def lcm(a, b):
+    """Least common multiple"""
+    return a * b // gcd(a, b)
+
+
+assert lcm(21, 6) == 42
+
+
+def solve_a(n):
     d = {}
     for i in range(2, n + 1):
         for (prime, power) in prime_fact(i).items():
@@ -45,7 +66,19 @@ def solve(n):
     return prod(p**c for (p, c) in d.items())
 
 
-assert solve(10) == 2520
+assert solve_a(10) == 2520
+
+
+def solve_b(n):
+    acc = 1
+    for i in range(2, n):
+        acc = lcm(acc, i)
+    return acc
+
+
+assert solve_b(10) == solve_a(10)
 
 if __name__ == '__main__':
-    print(solve(20))
+    for i in range(1, 3):
+        bench(solve_a, 10, 10**i)
+        bench(solve_b, 10, 10**i)
